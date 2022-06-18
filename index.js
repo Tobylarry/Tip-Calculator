3b2ae28067968370aad1f922e766d1b0ada63d7c
Tobi <47112773+Tobylarry@users.noreply.github.com>
2022-06-17 17:25:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,146 +1,45 @@
+'use strict'
+
 const payment = document.getElementById('payment');
-const five = document.getElementById('five');
-const ten = document.getElementById('ten');
-const fift = document.getElementById('fift');
-const tf = document.getElementById('tf');
-const fifty = document.getElementById('fifty');
 const custom = document.getElementById('custom');
 const tipValue = document.getElementById('tipValue');
-const customInp = document.getElementById('customInp');
-const submit = document.getElementById('Submit');
-const clos = document.getElementById('close');
-const modall = document.getElementById('mo');
 const people = document.getElementById('people');
 const total = document.getElementById('total');
 const reset = document.getElementById('reset');
-
-//disable reset button
-
-//Event Listeners
-five.addEventListener('click', fiveP)
-ten.addEventListener('click', tenP)
-fift.addEventListener('click', fiftP)
-tf.addEventListener('click', tfP)
-fifty.addEventListener('click', fiftyP)
-custom.addEventListener('click', displayModal)
-clos.addEventListener('click', closeM)
-submit.addEventListener('click', submitM)
-reset.addEventListener('click', resetPage)
+const tip = document.querySelectorAll('.tip');
 
 
-// method to calculate five %
-function fiveP() {
-    if (payment.value > 0 && people.value > 0) {
-        let tip = ((5 / 100) * payment.value);
-        let perP = (payment.value / people.value);
-        let totall = (tip + perP).toFixed(2);
-        five.style.color = "#00494D";
-        five.style.backgroundColor = "#26C0AB";
-        reset.style.backgroundColor = "#26C0AB"
-        reset.style.color = "#00494D";
-        total.innerHTML = "$ " + totall.toFixed(2);
-        tipValue.innerHTML = "$ " + tip.toFixed(2);
-    } else {
-        alert("Input approprate values")
-    }
+for(let i = 0; i < tip.length; i++){
+    tip[i].addEventListener('click', function(){
+        let x = Number(tip[i].getAttribute('data-value'));
+        calcTotal(x);
+    })
 }
 
-// method to calculate ten %
-function tenP() {
-    if (payment.value > 0 && people.value > 0) {
-        let tip = ((10 / 100) * payment.value);
-        let perP = (payment.value / people.value);
-        let totall = (tip + perP);
-        ten.style.color = "#00494D";
-        ten.style.backgroundColor = "#26C0AB";
-        reset.style.backgroundColor = "#26C0AB"
-        reset.style.color = "#00494D";
-        total.innerHTML = "$ " + totall.toFixed(2);
-        tipValue.innerHTML = "$ " + tip.toFixed(2);
-    } else {
-        alert("Input approprate values")
-    }
-}
-// method to calculate ten %
-function fiftP() {
-    if (payment.value > 0 && people.value > 0) {
-        let tip = ((15 / 100) * payment.value);
-        let perP = (payment.value / people.value);
-        let totall = (tip + perP);
-        fift.style.color = "#00494D";
-        fift.style.backgroundColor = "#26C0AB";
-        reset.style.backgroundColor = "#26C0AB"
-        reset.style.color = "#00494D";
-        total.innerHTML = "$ " + totall.toFixed(2);
-        tipValue.innerHTML = "$ " + tip.toFixed(2);
-    } else {
-        alert("Input approprate values")
-    }
-}
-// method to calculate ten %
-function tfP() {
-    if (payment.value > 0 && people.value > 0) {
-        let tip = ((25 / 100) * payment.value);
-        let perP = (payment.value / people.value);
-        let totall = (tip + perP);
-        tf.style.color = "#00494D";
-        tf.style.backgroundColor = "#26C0AB";
-        reset.style.backgroundColor = "#26C0AB"
-        reset.style.color = "#00494D";
-        total.innerHTML = "$ " + totall.toFixed(2);
-        tipValue.innerHTML = "$ " + tip.toFixed(2);
-    } else {
-        alert("Input approprate values")
-    }
-}
-// method to calculate ten %
-function fiftyP() {
-    if (payment.value > 0 && people.value > 0) {
-        let tip = ((50 / 100) * payment.value);
-        let perP = (payment.value / people.value);
-        let totall = (tip + perP);
-        fifty.style.color = "#00494D";
-        fifty.style.backgroundColor = "#26C0AB";
-        reset.style.backgroundColor = "#26C0AB"
-        reset.style.color = "#00494D";
-        total.innerHTML = "$ " + totall.toFixed(2);
-        tipValue.innerHTML = "$ " + tip.toFixed(2);
+//Event to reset page after clicking the reset button
+reset.addEventListener('click', () => location.reload());
 
-    } else {
-        alert("Input approprate values");
-    }
-}
-//cutom tip
+//cutom tip listen to the enter key and return values
+custom.addEventListener('keypress', (e) => {
+    (e.target === 'Enter') 
+    let x = Number(custom.value);
+    calcTotal(x)    
+})
 
-function displayModal() {
-    modall.style.display = 'block';
-}
-
-function closeM() {
-    modall.style.display = 'none';
-}
-
-function submitM() {
-    if (payment.value > 0 && customInp.value > 0 && people.value > 0) {
-        let tip = ((customInp.value / 100) * payment.value);
-        let perP = (payment.value / people.value);
+//Method takes bill, tip percentage and number of people
+// it return tip per person and total per person
+function calcTotal(x) {
+        let pay = Number(payment.value);
+        let ppl = Number(people.value); 
+        let tip = ((x / 100) * pay) / ppl;
+        let perP = (pay / ppl);
         let totall = (tip + perP);
         custom.style.backgroundColor = "#F4FAFA";
         custom.style.color = "#757575";
-        custom.style.border = "2px solid #26C0AB "
-        reset.style.backgroundColor = "#26C0AB"
+        custom.style.border = "2px solid #26C0AB";
+        reset.style.backgroundColor = "#26C0AB";
         reset.style.color = "#00494D";
-        total.innerHTML = "$ " + totall.toFixed(2);
-        tipValue.innerHTML = "$ " + tip.toFixed(2);
-        modall.style.display = 'none';
+        total.innerHTML = `$ ${totall.toFixed(2)}`;
+        tipValue.innerHTML = `$ ${tip.toFixed(2)}`;
 
-    } else {
-        modall.style.display = 'none';
-        alert("Input approprate values");
-    }
-
-}
-
-function resetPage() {
-    location.reload();
 }
